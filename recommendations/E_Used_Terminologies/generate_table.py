423c828f82b95df5e776ea70d7f9804ba2e279db
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-03-02 10:47:03 +0100

    Add recommendation on consortium ontology collections (#81)
    This PR adds an explicit recommendation that consortia should maintain
    the list of ontologies they use as collections in Semantic Farm
    
    preview the updated page at:
    https://github.com/nfdi-de/section-metadata-wg-onto/blob/recommendation-e/recommendations/E_Used_Terminologies/README.md
    
    builds on old [google
    sheet](https://docs.google.com/spreadsheets/d/1UAfDKo2gKiaFldEeitMUcO8Gl1Fjyb_r_bp1V4JW0Es)
    
    Closes https://github.com/nfdi-de/section-metadata-wg-onto/issues/43,
    relies on https://github.com/biopragmatics/bioregistry/pull/1816

---
+++ recommendations/E_Used_Terminologies/generate_table.py
@@ -0,0 +1,60 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "bioregistry>=0.13.20",
+#     "click>=8.3.1",
+#     "tabulate>=0.9.0",
+#     "pyperclip",
+# ]
+# ///
+
+"""Generate the NFDI consortium collections table."""
+
+import bioregistry
+import click
+import pyperclip
+from tabulate import tabulate
+
+NFDI_ROR = "05qj6w324"
+
+
+@click.command()
+def main() -> None:
+    rows = []
+    for collection in bioregistry.read_collections().values():
+        if any(
+            organization.ror == NFDI_ROR
+            for organization in collection.organizations or []
+        ):
+            org = next(org for org in collection.organizations if org.ror != NFDI_ROR)
+            authors = [
+                author
+                for author in collection.authors
+                if author.name != "Charles Tapley Hoyt"
+            ]
+
+            name_plus = org.name.replace(" ", "+")
+            suggest_link = f"https://github.com/biopragmatics/bioregistry/issues/new?template=add-collection-prefix.yml&collection={collection.identifier}&title=Add+prefix+X+to+{name_plus}"
+
+            rows.append(
+                (
+                    f"[{org.name}](https://semantic.farm/collection/{collection.identifier})",
+                    len(collection.resources),
+                    authors[0].name if authors else None,
+                    f"[Add prefix to collection]({suggest_link})",
+                )
+            )
+
+    if len(rows) != 26:  # as of feb 2026
+        raise ValueError(f"missing some NFDI consortia, only found {len(rows)}")
+
+    table = tabulate(
+        rows, tablefmt="github", headers=["Consortium", "#", "Contact", "Suggest"]
+    )
+    click.echo(table)
+    click.echo("\nthis table has been copied to your clipboard")
+    pyperclip.copy(table)
+
+
+if __name__ == "__main__":
+    main()
